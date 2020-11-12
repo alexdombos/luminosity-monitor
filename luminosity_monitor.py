11ef6546d36c66dfe33384327618202ed3ae0f49
Alex Dombos <adombos@nd.edu>
2020-11-12 10:32:42 -0500

    Apply formatting

--- luminosity_monitor.py
+++ luminosity_monitor.py
@@ -1,20 +1,27 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+def main():
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    video_capture = cv2.VideoCapture(filename = 'http://redacted-rbpi-hostname:8080/stream/video.mjpeg')
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while True:
+        # Capture frame-by-frame
+        frame_read_correctly, frame = video_capture.read()
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Our operations on the frame come here
+        image = cv2.cvtColor(src = frame, code = cv2.COLOR_BGR2GRAY)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+        # Display the resulting frame
+        cv2.imshow(winname = 'frame', mat = image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
